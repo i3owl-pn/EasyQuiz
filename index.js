d85768d174fb32abc4ce7d111ad74aedb13a2089
i3owl-pn <phromporn.n@gmail.com>
2024-12-15 10:22:30 +0700

    ชุดข้อสอบอย่างง่าย
    ชุดข้อสอบอย่างง่ายแต่การสรุปคะแนนยังมีบัค

---
+++ index.js
@@ -0,0 +1,31 @@
+let count = 0;
+function checkAnswer(answer) {
+    const result = document.getElementById('result');
+    if (answer === 'a') {
+    result.textContent = "ถูกต้อง PIM ย่อมาจาก PIM";
+    result.style.color = 'green';
+    count++;
+    } else {
+    result.textContent = "Incorrect! Try again.";
+    result.style.color = 'red';
+    }
+    }
+
+    function checkAnswer2(answer) {
+        const result = document.getElementById('result2');
+        const result3 = document.getElementById('result3');
+
+        if (answer === 'd') {
+        result.textContent = "ถูกต้อง มี 4 ตัวอักษร";
+        result.style.color = 'green';
+        count++;
+
+        } else {
+        result.textContent = "Incorrect! Try again.";
+        result.style.color = 'red';
+        }
+
+        result3.textContent = "คุณได้ทั้งหมด " + count + " คะแนน";
+       
+
+        }
